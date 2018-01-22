95b3dc796b479a06511dff97f5884e9af729ec56
edwin rivera <waehake@gmail.com>
2018-01-22 17:25:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+var pbj = {
+  bread: 'white',
+  ingredients: ['peanut butter', 'jelly'],
+  cut: 'triangles',
+  serve: function() {
+    console.log('here is your ' + this.name + ', enjoy');
+  },
+};
+
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  this.serve = function () {
+    console.log('here is your ' + this.name + ', enjoy');
+  };
+}
+
+var blt = new Sandwich('white', ['bacon', 'lettuce', 'tomato'], 'half');
+blt.name = "BLT"
+blt.serve();
+
+var reuben = new Sandwich('rye', ['corned beef', 'sauerkraut', 'russian dressing'], 'diagonal');
+
+
